ed7b4ea869a148369974e350b15be8eeab2a9b71
LGoulart <leandrovgoulart@gmail.com>
2024-05-28 19:53:33 -0300

    first commit

---
+++ src/main/java/com/goulart/leandro_LiterAlura/LeandroLiterAluraApplication.java
@@ -0,0 +1,13 @@
+package com.goulart.leandro_LiterAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeandroLiterAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeandroLiterAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goulart/leandro_LiterAlura/LeandroLiterAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goulart.leandro_LiterAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeandroLiterAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
