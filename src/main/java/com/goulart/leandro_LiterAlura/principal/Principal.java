aee2e13760ddd135e3eec050a9748e5a00008842
LGoulart <leandrovgoulart@gmail.com>
2024-06-04 11:34:55 -0300

    initial commit

---
+++ src/main/java/com/goulart/leandro_LiterAlura/principal/Principal.java
@@ -0,0 +1,11 @@
+package com.goulart.leandro_LiterAlura.principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Principal {
+    public static void main(String[] args) {
+        SpringApplication.run(Principal.class, args);
+    }
+}
